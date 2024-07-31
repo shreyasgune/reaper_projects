90ad6b57b8630f03fb6a4330cd3e89be6a478c78
shreyasgune <sgune@protonmail.com>
2024-07-30 21:48:57 -0400

    main -

--- backups.sh
+++ backups.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
+echo "hello there (general kenobi) and welcome to the gman backups"
+
+echo "taking git backup"
   BRANCH_NAME=`git symbolic-ref --short HEAD 2>/dev/null`
   COMMIT="$1"
   echo $COMMIT
   git add -A
   git commit -m "$BRANCH_NAME - $COMMIT"
   git push origin $BRANCH_NAME
-
+echo "git backup is done. now taking audio files backup"
   
   export GOOGLE_APPLICATION_CREDENTIALS="/mnt/e/gman-bucket-sa.json"
-  gsutil -p angelic-digit-297517 -m rsync -r /mnt/e/reaper_projects gs://gman-music/reaper_projects
+  gsutil -m rsync -r /mnt/e/reaper_projects gs://gman-music/reaper_projects
+
+echo "all backups, done"
